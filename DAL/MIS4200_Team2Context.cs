32de4b7569ee10f1544f1f3acf1016ad01a9a68f
Alex Olinger <ao188116@ohio.edu>
2020-11-06 16:30:37 -0500

    Started Recognitions Feature

--- DAL/MIS4200_Team2Context.cs
+++ DAL/MIS4200_Team2Context.cs
@@ -11,9 +11,11 @@ namespace MIS4200_Team2.DAL
     {
         public MIS4200_Team2Context() : base ("name=DefaultConnection")
         {
-         
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MIS4200_Team2Context, MIS4200_Team2.Migrations.MISContext.Configuration>("DefaultConnection"));
         }
 
         public DbSet<employeeData> employeeData { get; set;}
+
+        public System.Data.Entity.DbSet<MIS4200_Team2.Models.Recognition> Recognitions { get; set; }
     }
 }
---
+++ Models/Recognition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200_Team2.Models
+{
+    public class Recognition
+    {
+        public int ID { get; set; }
+        
+        [Display(Name ="Core Value Being Recognized")]
+        public CoreValue award { get; set; }
+
+        [Display(Name = "Who's giving the recognition?")]
+        public Guid recognizer { get; set; }
+
+        [Display(Name = "Receiver of recognition")]
+        public Guid recognized { get; set; }
+
+        [Display(Name = "Date recognition given")]
+        public DateTime recognizationDate { get; set; }
+
+        public enum CoreValue
+        {
+            Stewardship = 1,
+            Culture = 2,
+            Innovation = 3,
+            Integrity = 4,
+            Delivery = 5,
+            GreaterGood = 6,
+            Balance = 7
+        }
+    }
+}
